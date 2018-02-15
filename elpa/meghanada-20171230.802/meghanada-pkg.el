a822fbefe203208f5fe662857562fffebb42d6e2
Jan Ypma <jyp@tradeshift.com>
2018-02-15 11:54:19 +0100

    Putting elpa packages in git, as recommended
    https://www.reddit.com/r/emacs/comments/4fqu0a/automatically_install_packages_on_startup/

---
+++ elpa/meghanada-20171230.802/meghanada-pkg.el
@@ -0,0 +1,10 @@
+(define-package "meghanada" "20171230.802" "A better java development mode"
+  '((emacs "24.3")
+    (yasnippet "0.6.1")
+    (company "0.9.0")
+    (flycheck "0.23"))
+  :url "https://github.com/mopemope/meghanada-emacs" :keywords
+  '("languages" "java"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
