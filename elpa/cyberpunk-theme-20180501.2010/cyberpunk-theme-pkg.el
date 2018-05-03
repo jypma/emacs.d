b93157d3d587af357e09e98d8b5107bc3963fb8d
Jan Ypma <jyp@tradeshift.com>
2018-05-03 14:47:01 +0200

    Switch to cyberpunk theme, with small changes

---
+++ elpa/cyberpunk-theme-20180501.2010/cyberpunk-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cyberpunk-theme" "20180501.2010" "Cyberpunk Color Theme" 'nil :commit "2f12f08489d88efdb3ff6ca9489ef819f0f3b428" :keywords '("color" "theme" "cyberpunk"))
