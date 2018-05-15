edf53524a9a7267cbc4b5ca68aa8ae804b79f6a0
jypma <jan@ypmania.nl>
2018-05-15 20:35:27 +0200

    Quote literal on remote dired so that renames work spaces
    Without it, the default quoting mode of "shell-escape" apparently puts
    single quotes around file names with spaces, but only for remote
    buffers. Strange.

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (misterioso)))
+ '(dired-listing-switches "-al --quoting-style=literal")
  '(fill-column 110)
  '(git-gutter:update-interval 1)
  '(global-subword-mode t)
