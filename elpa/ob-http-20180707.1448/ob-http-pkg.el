5e4612557454a4d9fa6186e429ce7d837ca31e77
Jan Ypma <jyp@tradeshift.com>
2018-12-13 09:15:45 +0100

    Add dockerfile editing and org-babel http support

---
+++ elpa/ob-http-20180707.1448/ob-http-pkg.el
@@ -0,0 +1,11 @@
+(define-package "ob-http" "20180707.1448" "http request in org-mode babel"
+  '((s "1.9.0")
+    (cl-lib "0.5"))
+  :authors
+  '(("ZHOU Feng" . "zf.pascal@gmail.com"))
+  :maintainer
+  '("ZHOU Feng" . "zf.pascal@gmail.com")
+  :url "http://github.com/zweifisch/ob-http")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
