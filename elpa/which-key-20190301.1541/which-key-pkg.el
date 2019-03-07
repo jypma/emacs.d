d45c8372e7e3c33819a677ac9b48f30e983b186e
Jan Ypma <jyp@tradeshift.com>
2019-03-07 09:35:05 +0100

    Another full upgrade since lsp-java wasn't quite running smoothly.

--- elpa/which-key-20190301.1541/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "which-key" "20190301.1541" "Display available keybindings in popup" '((emacs "24.4")) :commit "51c485f16b4724488bcf2cbf7933fbd9eede1edd" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
