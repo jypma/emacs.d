cd867d49ced83242253e9d36b65a1caf0ff0294c
Jan Ypma <jyp@tradeshift.com>
2019-03-27 09:04:24 +0100

    Yasnippet updates

--- init.el
+++ init.el
@@ -811,6 +811,9 @@ See `elfeed-play-with-mpv'."
   :ensure t
   :config
   (yas-global-mode 1)
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "TAB") nil)
+  (define-key yas-minor-mode-map (kbd "C-t") #'yas-expand)
   :delight yas-minor-mode)
 
 (use-package highlight-symbol
