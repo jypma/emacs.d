6b612c6132fa28852bd7188cc9f2903312a22a19
Jan Ypma <jyp@tradeshift.com>
2019-06-19 09:44:56 +0200

    Don't ever auto-autocomplete

--- init.el
+++ init.el
@@ -29,7 +29,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-auto-complete (quote (quote company-explicit-action-p)))
+ '(company-auto-complete nil)
  '(company-auto-complete-chars (quote (32 95 40 41 46)))
  '(company-idle-delay 0.3)
  '(company-lsp-enable-recompletion nil)
