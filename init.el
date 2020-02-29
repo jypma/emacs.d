2bbe527d319839530292af62f0693ef9a55c8d33
jypma <jan@ypmania.nl>
2020-02-29 17:35:56 +0100

    metals emacs updates as per updated website

--- init.el
+++ init.el
@@ -205,7 +205,10 @@
   (substitute-key-definition
    'minibuffer-complete-word
    'self-insert-command
-   minibuffer-local-completion-map))
+   minibuffer-local-completion-map)
+
+  ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+  (setq sbt:program-options '("-Dsbt.supershell=false")))
 
 (use-package scala-mode
   :mode "\\.s\\(cala\\|bt\\)$"
