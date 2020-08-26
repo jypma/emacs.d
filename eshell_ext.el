23e1ae9273f9c6bd8874333c4ff998887730be95
jypma <jan@ypmania.nl>
2020-08-26 14:00:30 +0200

    Add function to look up the XML path within a large document

--- eshell_ext.el
+++ eshell_ext.el
@@ -354,3 +354,22 @@ by using nxml's indentation rules."
     (while (search-forward-regexp "\>[ \\t]*\<" nil t)
       (backward-char) (insert "\n"))
     (indent-region begin end)))
+
+;; from https://www.emacswiki.org/emacs/NxmlMode#toc11
+(defun nxml-where ()
+  "Display the hierarchy of XML elements the point is on as a path."
+  (interactive)
+  (let ((path nil))
+    (save-excursion
+      (save-restriction
+        (widen)
+        (while (and (< (point-min) (point)) ;; Doesn't error if point is at beginning of buffer
+                    (condition-case nil
+                        (progn
+                          (nxml-backward-up-element) ; always returns nil
+                          t)
+                      (error nil)))
+          (setq path (cons (xmltok-start-tag-local-name) path)))
+        (if (called-interactively-p t)
+            (message "/%s" (mapconcat 'identity path "/"))
+          (format "/%s" (mapconcat 'identity path "/")))))))
