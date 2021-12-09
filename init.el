c868ece86dea009914e2eddb3041bc3d8121ae81
Jan Ypma <jan@ypmania.nl>
2021-12-09 13:53:29 +0100

    Add gdscript mode

--- init.el
+++ init.el
@@ -1412,3 +1412,30 @@ See `elfeed-play-with-mpv'."
 (unless (server-running-p) (server-start))
 
 (require 'pkgbuild-mode)
+
+(use-package gdscript-mode
+  :hook (gdscript-mode . lsp)
+  :bind
+  (:map gdscript-mode-map
+        ("<f5>" . nil)
+        ("<f6>" . nil)
+        ("<f7>" . nil)
+        ("<f8>" . nil)
+        ("<f9>" . nil))
+  :config
+
+
+  ;; https://github.com/godotengine/emacs-gdscript-mode#known-issues
+  (defun lsp--gdscript-ignore-errors (original-function &rest args)
+    "Ignore the error message resulting from Godot not replying to the `JSONRPC' request."
+    (if (string-equal major-mode "gdscript-mode")
+        (let ((json-data (nth 0 args)))
+          (if (and (string= (gethash "jsonrpc" json-data "") "2.0")
+                   (not (gethash "id" json-data nil))
+                   (not (gethash "method" json-data nil)))
+              nil ; (message "Method not found")
+            (apply original-function args)))
+      (apply original-function args)))
+  ;; Runs the function `lsp--gdscript-ignore-errors` around `lsp--get-message-type` to suppress unknown notification errors.
+  (advice-add #'lsp--get-message-type :around #'lsp--gdscript-ignore-errors))
+
