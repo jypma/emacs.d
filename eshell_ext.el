d93accc8322128c27f23ae39513138369dfddeb5
Jan Ypma <jan@ypmania.nl>
2022-04-26 13:11:23 +0200

    Tweak OSM, add randomize region

--- eshell_ext.el
+++ eshell_ext.el
@@ -423,3 +423,15 @@ By default, all subentries are counted; restrict with LEVEL."
                              (or (not maxlevel)
                                  (<= (org-current-level) maxlevel)))
                            match (or scope 'tree)))))))))
+
+  (defun my-randomize-region (beg end)
+    "Randomize lines in region from BEG to END."
+    (interactive "*r")
+    (let ((lines (split-string
+                   (delete-and-extract-region beg end) "\n")))
+      (when (string-equal "" (car (last lines 1)))
+        (setq lines (butlast lines 1)))
+      (apply 'insert
+        (mapcar 'cdr
+        (sort (mapcar (lambda (x) (cons (random) (concat x "\n"))) lines)
+              (lambda (a b) (< (car a) (car b))))))))
